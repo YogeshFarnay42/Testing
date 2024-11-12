10dfd978a3e4a5a05c511fc686a9c55d2084e1c5
yogeshfarnay@gmail.com <yogeshfarnay@gmail.com>
2024-11-12 11:34:35 +0530

    2 commit

---
+++ src/java_Pratice_25Oct2024/forth.java
@@ -0,0 +1,17 @@
+package java_Pratice_25Oct2024;
+
+public class forth {
+
+	public static void main(String[] args) {
+		int a=90;
+		int b= 80;
+		
+		int c= a+b;
+		
+		System.out.println("total " + c);
+		
+		
+
+	}
+
+}
